dafc0e175460dcc66e834a8b91230ccba185553e
HanzlaLatitude <hanzalasaadi@gmail.com>
2024-06-23 19:21:40 +0500

    feat: Add json-server for mock API and fetch questions from API with useReducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import DateCounter from "./DateCounter";
+import Header from "./Header";
+import Main from "./Main";
+import React, { useEffect, useReducer } from "react";
+
+const initialState = {
+  questions: [],
+
+  // loading, fetched, error, active, finished
+  state: "loading",
+};
+
+const reducer = function (currState, action) {
+  switch (action.type) {
+    case "fetched":
+      return { ...currState, state: "fetched", questions: action.payload };
+    case "error":
+      return { ...currState, state: "error" };
+    default:
+      throw new Error("No Action Found");
+  }
+};
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    async function fetchQuestions() {
+      try {
+        const data = await fetch("http://localhost:3001/questions");
+        const res = await data.json();
+        dispatch({ type: "fetched", payload: res });
+      } catch (error) {
+        dispatch({ type: "error" });
+      }
+    }
+    fetchQuestions();
+  }, []);
   return (
     <div className="App">
-      <DateCounter />
+      <Header />
+
+      <Main>
+        <p>1/15</p>
+        <p>Question: </p>
+      </Main>
     </div>
   );
 }
