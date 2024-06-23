51d892e22de246d7a71c82bb6d5e9c7df1cc9402
HanzlaLatitude <hanzalasaadi@gmail.com>
2024-06-23 23:54:12 +0500

    chore: Update Question component to render question and options

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,5 +1,17 @@
 function Question({ question, dispatch }) {
-  return <div></div>;
+  return (
+    <div>
+      <h4>{question.question}</h4>
+
+      <div className="options">
+        {question.options.map((opt, i) => (
+          <button key={opt} className="btn btn-option">
+            {opt}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Question;
