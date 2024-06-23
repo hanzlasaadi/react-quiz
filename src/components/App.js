17db48123987f2f7f020444b2af657f9d6d507f6
HanzlaLatitude <hanzalasaadi@gmail.com>
2024-06-24 00:22:24 +0500

    chore: Handle submitting answers for a question

--- src/components/App.js
+++ src/components/App.js
@@ -8,28 +8,38 @@ import Question from "./Question";
 
 const initialState = {
   questions: [],
-
   // loading, fetched, error, active, finished
   state: "loading",
-
+  // index of current question
   index: 0,
+  // array of answers for each question
+  answers: [],
 };
 
 const reducer = function (currState, action) {
   switch (action.type) {
     case "fetched":
-      return { ...currState, state: "fetched", questions: action.payload };
+      return {
+        ...currState,
+        state: "fetched",
+        questions: action.payload,
+        answers: new Array(action.payload.length),
+      };
     case "error":
       return { ...currState, state: "error" };
     case "start":
       return { ...currState, state: "active" };
+    case "submitAnswer":
+      let copyArr = [...currState.answers];
+      copyArr[currState.index] = action.payload;
+      return { ...currState, answers: copyArr };
     default:
       throw new Error("No Action Found");
   }
 };
 
 function App() {
-  const [{ questions, state, index }, dispatch] = useReducer(
+  const [{ questions, answers, state, index }, dispatch] = useReducer(
     reducer,
     initialState
   );
@@ -57,7 +67,11 @@ function App() {
           <StartScreen length={questions.length} dispatch={dispatch} />
         )}
         {state === "active" && (
-          <Question question={questions[index]} dispatch={dispatch} />
+          <Question
+            question={questions[index]}
+            answered={answers[index]}
+            dispatch={dispatch}
+          />
         )}
       </Main>
     </div>
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,17 @@
-function Question({ question, dispatch }) {
+function Question({ question, dispatch, answered }) {
   return (
     <div>
       <h4>{question.question}</h4>
 
       <div className="options">
         {question.options.map((opt, i) => (
-          <button key={opt} className="btn btn-option">
+          <button
+            key={opt}
+            className={`btn btn-option ${
+              answered !== undefined && answered === i ? "answer" : ""
+            }`}
+            onClick={() => dispatch({ type: "submitAnswer", payload: i })}
+          >
             {opt}
           </button>
         ))}
